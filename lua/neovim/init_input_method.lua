6dae571a157d3ad8426cc8cbdeba087e6e80f9aa
LongyuXie <longyuxie266@gmail.com>
2024-04-20 21:30:05 +0800

    feat: add system type check for fcitx5-remote

--- lua/neovim/init_input_method.lua
+++ lua/neovim/init_input_method.lua
@@ -1,11 +1,15 @@
 
-vim.api.nvim_create_autocmd(
-    { "InsertLeave" },
-    {
-        pattern = {"*"},
-        callback = function ()
-            os.execute("fcitx5-remote -c")
-        end,
-        desc = "inactivate fcitx5 when leaving insert mode"
-    }
-)
+local sysname = vim.loop.os_uname().sysname
+
+if sysname == "Linux" then
+    vim.api.nvim_create_autocmd(
+        { "InsertLeave" },
+        {
+            pattern = {"*"},
+            callback = function ()
+                os.execute("fcitx5-remote -c")
+            end,
+            desc = "inactivate fcitx5 when leaving insert mode"
+        }
+    )
+end
